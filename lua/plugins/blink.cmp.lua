d996b01dfaa256b5ad337532020f68812de669d0
Bogdan Lazar <bogdan@bogdanlazar.com>
2025-02-05 17:54:34 +0100

    Add blink.cmp config

---
+++ lua/plugins/blink.cmp.lua
@@ -0,0 +1,22 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "none",
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+    },
+    completion = {
+      list = {
+        selection = {
+          preselect = false,
+        },
+      },
+    },
+  },
+}
