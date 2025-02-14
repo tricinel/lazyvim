2933a6c9072ce2f022e1252c4c60b9ef68ecb607
Bogdan Lazar <bogdan@bogdanlazar.com>
2025-02-14 10:15:28 +0100

    Update keymaps and add Oil

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,14 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.winbar = "%=%m %f"
-vim.opt.scrolloff = 8 -- Lines of context
-vim.opt.guicursor = ""
 
-vim.opt.scrolloff = 10
+local opt = vim.opt
+
+opt.winbar = "%=%m %f"
+opt.scrolloff = 20 -- Lines of context
+opt.guicursor = ""
 
 -- Don't put new windows below current
-vim.opt.splitbelow = false
+opt.splitbelow = false
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
